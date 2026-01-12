f960b2cb55b758a979e9208a4792a60684c026d6
maxinemuster <max.muster@kaffeeschluerfer.com>
2026-01-12 10:29:20 +0100

    fix typo - comment was not propper formatted

--- src/hal/xradio/hal_wifi_xradio.c
+++ src/hal/xradio/hal_wifi_xradio.c
@@ -94,12 +94,12 @@ int HAL_SetupWiFiAccessPoint(const char *ssid, const char *key) {
 
 	net_switch_mode(WLAN_MODE_HOSTAP);
 	wlan_ap_disable();
-
-/	// not present in XR809
+/*
+	// not present in XR809
 	wlan_ap_default_conf_t *conf;
 	conf=(wlan_ap_default_conf_t *)wlan_ap_get_default_conf();
 	conf->channel=g_wifi_channel;
-*
+*/
 	wlan_ap_set((uint8_t *)ssid, strlen(ssid), (uint8_t *)key);
 	wlan_ap_config_t config;
 	memset(&config, 0, sizeof(config));
